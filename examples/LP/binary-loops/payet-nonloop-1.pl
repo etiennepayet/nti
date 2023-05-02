4c4438ed5f3eb82edd83ee511c7550ae6ae300f5
etiennepayet <etienne.payet@univ-reunion.fr>
2023-05-02 09:17:57 +0400

    Improved output

--- examples/LP/binary-loops/payet-nonloop-1.pl
+++
@@ -1,4 +0,0 @@
-% The following mode is lasso-looping.
-%query: p(i,i).
-p(0, Y) :- p(s(Y), s(Y)).
-p(s(X), Y) :- p(X, Y).
--- examples/LP/binary-loops/payet-nonloop-2.pl
+++
@@ -1,6 +0,0 @@
-% The following mode is lasso-looping.
-%query: p(i,i).
-p(0, Y) :- p(s(Y), s(Y)).
-p(s(X), Y) :- q(X), p(X, Y).
-q(0).
-q(s(_)).
